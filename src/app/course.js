b3220e952e6e79c5f157eec7cc99c87f50d4ed45
samettyldrm <smtyldrm32@gmail.com>
2024-01-24 00:27:08 +0100

    update

---
+++ src/app/course.js
@@ -0,0 +1,25 @@
+// export default function Course() {
+//     return (
+//         <div>
+//             Kurslarım
+//         </div>
+//     );
+// }
+
+// const Course = () => {
+//   return (
+//     <div>
+//       Kurslarım
+//     </div>
+//   )
+// }
+
+// export default Course
+
+function Course() {
+  return ( <div>deneme</div> );
+}
+
+export default Course;
+
+
