47a8c9441230af92b69b0c294dab10f5764b4330
samettyldrm <smtyldrm32@gmail.com>
2024-01-30 03:38:40 +0100

    Update page.js

--- src/app/ApiCall/page.js
+++ src/app/ApiCall/page.js
@@ -1,23 +1,22 @@
-'use client'
+"use client";
 
 import SearchHeader from "./SearchHeader";
 import searchImages from "./Api";
-import {useState} from "react";
-import ImageList from "./ImageList"
-
+import { useState } from "react";
+import ImageList from "./ImageList";
 
 export default function App() {
-  const [images, setImages] = useState([])
-  
+  const [images, setImages] = useState([]);
+
   const handleSubmit = async (term) => {
     const result = await searchImages(term);
-    setImages(result)
-    console.log(images)
+    setImages(result);
+    console.log(images);
   };
   return (
     <div className="App">
       <SearchHeader search={handleSubmit} />
-      <ImageList imagesPlaceHolder = {images}/>
+      <ImageList imagesPlaceHolder={images} />
     </div>
   );
 }
